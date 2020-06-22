8aeeb29ea858bda23b73b9fa75696f9376ff81b0
Matticusau <matt.lavery@outlook.com>
2020-06-22 13:14:56 +1000

    Fixes #17

--- src/prwelcomehandler.ts
+++ src/prwelcomehandler.ts
@@ -6,6 +6,7 @@
 // When         Who         What
 // ------------------------------------------------------------------------------------------
 // 2020-06-20   MLavery     Config moved back to workflow file #3
+// 2020-06-22   Mlavery     Fixed multiple comment message #17
 //
 
 import { CoreModule, GitHubModule, Context } from './types' // , Client
@@ -14,57 +15,61 @@ import { PRHelper, MessageHelper } from './classes';
 export default async function prWelcomeHandler(core: CoreModule, github: GitHubModule) {
 
   try {
-    const prhelper = new PRHelper;
-    const messagehelper = new MessageHelper;
-    const prnumber = prhelper.getPrNumber(github.context);
-    if (!prnumber) {
-      core.info('Could not get pull request number from context, exiting');
-      return;
-    }
-    core.info(`Processing PR ${prnumber}!`);
+    // only on new PR
+    if (github.context.eventName === 'pull_request' 
+        && github.context.payload.action === 'opened') {
+
+      const prhelper = new PRHelper;
+      const messagehelper = new MessageHelper;
+      const prnumber = prhelper.getPrNumber(github.context);
+      if (!prnumber) {
+        core.info('Could not get pull request number from context, exiting');
+        return;
+      }
+      core.info(`Processing PR ${prnumber}!`);
   
-    const welcomeMessage = core.getInput('welcome-message');
-    const myToken = core.getInput('repo-token');
-    const octokit = github.getOctokit(myToken);
+      const welcomeMessage = core.getInput('welcome-message');
+      const myToken = core.getInput('repo-token');
+      const octokit = github.getOctokit(myToken);
 
-    // check if the welcome message is to be processed
-    if (core.getInput('enable-welcomemessage') === 'true') {
+      // check if the welcome message is to be processed
+      if (core.getInput('enable-welcomemessage') === 'true') {
 
-      // check if this is a new PR
-      // if (github.context.eventName === 'pull_request' && github.context.payload.action !== 'opened') {
-      //   core.debug('No issue or pull request was opened, skipping');
-      //   return;
-      // }
+        // check if this is a new PR
+        // if (github.context.eventName === 'pull_request' && github.context.payload.action !== 'opened') {
+        //   core.debug('No issue or pull request was opened, skipping');
+        //   return;
+        // }
+        
+        // add the welcome message if needed
+        // check if this is a new PR
+        if (github.context.eventName === 'pull_request' 
+          && github.context.payload.action === 'opened'
+          && welcomeMessage.length > 0) {
+          
+          // const octokit = github.getOctokit(myToken);
+          await octokit.issues.createComment({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            issue_number: prnumber,
+            body: welcomeMessage
+          });
+        }
+      }
       
-      // add the welcome message if needed
-      // check if this is a new PR
-      if (github.context.eventName === 'pull_request' 
-        && github.context.payload.action === 'opened'
-        && welcomeMessage.length > 0) {
+      
+      // check if we should add the comment automation message
+      if (core.getInput('enable-prcomment-automation') === 'true') {
         
         // const octokit = github.getOctokit(myToken);
         await octokit.issues.createComment({
           owner: github.context.repo.owner,
           repo: github.context.repo.repo,
           issue_number: prnumber,
-          body: welcomeMessage
+          body: messagehelper.prcommentautomationwelcome
         });
       }
-    }
-      
-      
-    // check if we should add the comment automation message
-    if (core.getInput('enable-prcomment-automation') === 'true') {
-      
-      // const octokit = github.getOctokit(myToken);
-      await octokit.issues.createComment({
-        owner: github.context.repo.owner,
-        repo: github.context.repo.repo,
-        issue_number: prnumber,
-        body: messagehelper.prcommentautomationwelcome
-      });
-    }
-    
+    }    
   }
   catch (error) {
     core.setFailed(error.message);
